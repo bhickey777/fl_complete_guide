2a387aa9d071f309085d866a2e97ad3f7793b32c
bhickey777 <90266950+bhickey777@users.noreply.github.com>
2021-09-14 18:44:32 -0500

    v2

--- lib/main.dart
+++ lib/main.dart
@@ -1,15 +1,15 @@
 import 'package:flutter/material.dart';
 
-void main() {
-  runApp(MyApp());
-}
+void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
-  //MyApp({Key? key}) : super(key: key);
+  //const MyApp({Key? key}) : super(key: key);
 
+  @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      home: Text('Hello!'),
+    var materialApp = MaterialApp(
+      home: const Text('Hello Doug!'),
     );
+    return Center(child: materialApp);
   }
 }
