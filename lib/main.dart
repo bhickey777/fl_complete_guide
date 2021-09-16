d271d26c637eab4a69a108076f1259300852a3ff
bhickey777 <90266950+bhickey777@users.noreply.github.com>
2021-09-15 19:53:10 -0500

    v4 - questions and buttons

--- lib/main.dart
+++ lib/main.dart
@@ -7,10 +7,21 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    var questionsAndActions = [
+      Text('What\'s your favorite color?'),
+      RaisedButton(child: Text('Answer 1'), onPressed: null),
+      RaisedButton(child: Text('Answer 2'), onPressed: null),
+      RaisedButton(child: Text('Answer 3'), onPressed: null),
+      Text('What\'s your favorite animal?'),
+      RaisedButton(child: Text('Answer 1'), onPressed: null),
+      RaisedButton(child: Text('Answer 2'), onPressed: null),
+      RaisedButton(child: Text('Answer 3'), onPressed: null),
+    ];
+
     return MaterialApp(
         home: Scaffold(
       appBar: AppBar(title: Text('My First App')),
-      body: Text('This is my default body!'),
+      body: Column(children: questionsAndActions),
     ));
   }
 }
